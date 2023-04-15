621bd031a7e5ac4d544aa82c4cf18d5a078c315e
luanrem <luanrem@gmail.com>
2023-04-14 23:00:18 -0300

    feat: create middleware json

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import http from "node:http";
 import { routes } from "./routes.js";
+import { json } from "./middlewares/json.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  await json(req, res);
 
   const route = routes.find((route) => {
     return route.method === method;
   });
   console.log(route);
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+    console.log(routeParams);
+  }
 
   return res.writeHead(404).end();
 });
