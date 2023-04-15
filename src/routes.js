7cdaf9243d2d329f29946e87e264d79770b353f3
luanrem <luanrem@gmail.com>
2023-04-15 19:38:05 -0300

    feat: Complete routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,35 @@
 export const routes = [
   {
+    method: "POST",
+    path: "/tasks",
+    handler: (req, res) => {
+      return res.end();
+    },
+  },
+  {
     method: "GET",
     path: "/tasks",
+    handler: (req, res) => {
+      return res.end();
+    },
+  },
+  {
+    method: "PUT",
+    path: "/tasks/:id",
+    handler: (req, res) => {
+      return res.end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/tasks/:id",
+    handler: (req, res) => {
+      return res.end();
+    },
+  },
+  {
+    method: "PATCH",
+    path: "/tasks/:id/complete",
     handler: (req, res) => {
       return res.end();
     },
